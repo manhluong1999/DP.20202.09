1620b87552982bc5b1c3f6ca76645fed42e919ad
hoangmanh <hoangmanhh99@gmail.com>
2021-03-28 22:33:02 +0700

    signleton SessionInfomation

--- src/main/java/controller/SessionInformation.java
+++ src/main/java/controller/SessionInformation.java
@@ -9,29 +9,30 @@ import java.time.LocalDateTime;
  * @author
  */
 /*
-*  Coincidental Cohesion: Vì các phần thử trong lớp không liên quan đến nhau
-* */
+ * Coincidental Cohesion: Vì các phần thử trong lớp không liên quan
+ * đến nhau
+ */
 //DP: Singleton pattern vi SessionInformation chi nen duoc tao ra chi 1 instance trong qua trinh chay chuong trinh
 public class SessionInformation {
 
- private User mainUser;
- private Cart cartInstance = Cart.getInstance();
- private LocalDateTime expiredTime;
- private static SessionInformation instance;
- 
- private SessionInformation() {
- 	cartInstance = Cart.getInstance();
- }
- 
- public static SessionInformation getInstance() {
- 	if (instance == null) {
- 		instance = new SessionInformation();
- 	}
- 	return instance;
- }
- 
- public User getMainUser() {
- 	return mainUser;
- }
+	private User mainUser;
+	private Cart cartInstance = Cart.getInstance();
+	private LocalDateTime expiredTime;
+	private static SessionInformation instance;
+
+	private SessionInformation() {
+		cartInstance = Cart.getInstance();
+	}
+
+	public static SessionInformation getInstance() {
+		if (instance == null) {
+			instance = new SessionInformation();
+		}
+		return instance;
+	}
+
+	public User getMainUser() {
+		return mainUser;
+	}
 
 }
